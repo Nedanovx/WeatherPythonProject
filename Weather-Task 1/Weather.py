c1003630b80151471c51eb4e22289de02117ab98
Nikola Nedanov <nedanovx@gmail.com>
2025-08-02 17:03:50 +0300

    Get 5 random cities with Name and information about temperature, humidity and description

--- Weather-Task 1/Weather.py
+++ Weather-Task 1/Weather.py
@@ -3,7 +3,7 @@
 ***REMOVED***
 ***REMOVED***
 API_KEY = '***REMOVED***'
-***REMOVED***
+URL = 'http://api.openweathermap.org/data/2.5/weather?'
 
 with gzip.open("city.list.json.gz", "rt", encoding="utf-8") as cities:
     all_cities = json.load(cities)
@@ -11,5 +11,31 @@ with gzip.open("city.list.json.gz", "rt", encoding="utf-8") as cities:
 ***REMOVED***
 
 ***REMOVED***
-***REMOVED***
+        city_id = city['id']
+        city_name = city['name']
+
+        params = {
+            'id': city_id,
+            'appid': API_KEY,
+            'units': 'metric',
+            'lang': 'en'
+        }
+        try:
+            response = requests.get(URL, params=params)
+            response.raise_for_status()
+            data = response.json()
+
+            description = data['weather'][0]['description']
+            temp = data['main']['temp']
+            humidity = data['main']['humidity']
+
+            print(f"city: {city_name}")
+            print(f"The weather is: {description}")
+            print(f"The temperature is: {temp}°C") # alt+0176
+            print(f"Humidity is: {humidity}%")
+            print()
+
+        except requests.RequestException as e:
+            print(e)
+
 
