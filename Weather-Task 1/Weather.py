0c34c6ad94857f0b02895d827eee68435280873a
Nikola Nedanov <nedanovx@gmail.com>
2025-08-02 18:42:45 +0300

    Added dictionary to find the coldest city

--- Weather-Task 1/Weather.py
+++ Weather-Task 1/Weather.py
@@ -5,6 +5,7 @@
 API_KEY = '***REMOVED***'
 URL = 'http://api.openweathermap.org/data/2.5/weather?'
 
+current_five_cities = {}
 with gzip.open("city.list.json.gz", "rt", encoding="utf-8") as cities:
     all_cities = json.load(cities)
 
@@ -29,13 +30,16 @@ with gzip.open("city.list.json.gz", "rt", encoding="utf-8") as cities:
             temp = data['main']['temp']
             humidity = data['main']['humidity']
 
+            current_five_cities[city_name] = temp
+            sorted_cities = dict(sorted(current_five_cities.items(), key=lambda x: x[1]))
             print(f"city: {city_name}")
             print(f"The weather is: {description}")
             print(f"The temperature is: {temp}°C") # alt+0176
             print(f"Humidity is: {humidity}%")
             print()
-
         except requests.RequestException as e:
             print(e)
-
+    city, temp = next(iter(sorted_cities.items()))
+    print(f"Coldest city: {city}")
+    print(f"Temperature: {temp}°C")
 
