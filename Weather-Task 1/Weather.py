0f85f96de5694e17897ed8544436ad7a75636f5c
Nikola Nedanov <nedanovx@gmail.com>
2025-08-02 15:57:18 +0300

    Get 5 random cities

---
+++ Weather-Task 1/Weather.py
@@ -0,0 +1,15 @@
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+API_KEY = '***REMOVED***'
+***REMOVED***
+
+with gzip.open("city.list.json.gz", "rt", encoding="utf-8") as cities:
+    all_cities = json.load(cities)
+
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+
