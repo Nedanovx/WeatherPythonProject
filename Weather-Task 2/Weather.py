673abe88a9d76e76f4d8b3258f5e1f79a44dc295
Nikola Nedanov <nedanovx@gmail.com>
2025-08-07 21:26:51 +0300

    Added def to get 5 random city

--- Weather-Task 2/Weather.py
+++ Weather-Task 2/Weather.py
@@ -2,7 +2,6 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-import statistics
 from dotenv import load_dotenv
 import os
 
@@ -11,76 +10,33 @@ load_dotenv()
 KEY = os.getenv('API_KEY')
 URL = 'http://api.openweathermap.org/data/2.5/weather?'
 
-current_five_cities = {}
-with gzip.open("city.list.json.gz", "rt", encoding="utf-8") as cities:
-    all_cities = json.load(cities)
 
-***REMOVED***
+def get_five_cities():
+    current_five_cities = {}
+    with gzip.open("city.list.json.gz", "rt", encoding="utf-8") as cities:
+        all_cities = json.load(cities)
 
-***REMOVED***
-        city_id = city['id']
-        city_name = city['name']
+    ***REMOVED***
+    ***REMOVED***
+            city_id = city['id']
+            city_name = city['name']
+            params = {
+                'id': city_id,
+                'appid': KEY,
+                'units': 'metric',
+                'lang': 'en'
+            }
+            try:
+                response = requests.get(URL, params=params)
+                response.raise_for_status()
+                data = response.json()
 
-        params = {
-            'id': city_id,
-            'appid': KEY,
-            'units': 'metric',
-            'lang': 'en'
-        }
-        try:
-            response = requests.get(URL, params=params)
-            response.raise_for_status()
-            data = response.json()
+                current_five_cities[city_name] = {
+                    "temp": data["main"]["temp"],
+                    "desc": data["weather"][0]["description"],
+                    "humidity": data["main"]["humidity"]
+                }
 
-            description = data['weather'][0]['description']
-            temp = data['main']['temp']
-            humidity = data['main']['humidity']
-
-            current_five_cities[city_name] = temp
-            sorted_cities = dict(sorted(current_five_cities.items(), key=lambda x: x[1]))
-            print(f"city: {city_name}")
-            print(f"The weather is: {description}")
-            print(f"The temperature is: {temp}°C") # alt+0176
-            print(f"Humidity is: {humidity}%")
-            print()
-        except requests.RequestException as e:
-            print(e)
-    city, temp = next(iter(sorted_cities.items()))
-    print(f"Coldest city: {city}")
-    print(f"Temperature: {temp}°C")
-    print(f"Average temperatures: {statistics.mean(sorted_cities.values()):.2f}°C")
-
-user_input = input("Which city would you like to see? ").strip().lower()
-
-isFound = False
-
-for city in  all_cities:
-    if city['name'].lower() == user_input:
-        city_id = city['id']
-        city_name = city['name']
-        isFound = True
-        break
-if isFound:
-    params = {
-        'id': city_id,
-        'appid': KEY,
-        'units': 'metric',
-        'lang': 'en'
-    }
-    try:
-        response = requests.get(URL, params=params)
-        response.raise_for_status()
-        data = response.json()
-
-        description = data['weather'][0]['description']
-        temp = data['main']['temp']
-        humidity = data['main']['humidity']
-
-        print(f"city: {city_name}")
-        print(f"The weather is: {description}")
-        print(f"The temperature is: {temp}°C")
-        print(f"Humidity is: {humidity}%")
-    except requests.RequestException as e:
-        print(e)
-else:
-    print(f'city: {user_input} not found')
+            except requests.RequestException as e:
+                print(e)
+        return current_five_cities
