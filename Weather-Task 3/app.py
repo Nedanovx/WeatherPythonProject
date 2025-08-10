d38d357845a04670e117da21e9de04733dc17bfd
Nikola Nedanov <107359038+Nedanovx@users.noreply.github.com>
2025-08-10 23:02:36 +0300

    added search route

--- Weather-Task 3/app.py
+++ Weather-Task 3/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from Weather import get_five_cities
+from flask import Flask, render_template,request
+from Weather import get_five_cities,search_city
 
 app = Flask(__name__)
 
@@ -7,6 +7,16 @@ app = Flask(__name__)
 def home():
     cities = get_five_cities()
     return render_template("index.html", cities=cities)
+
+@app.route("/search")
+def search():
+    city_name = request.args.get("city")
+    city_data = search_city(city_name)
+
+    if not city_data:
+        return render_template("error.html", error=f"City '{city_name}' not found"), 404
+
+    return render_template("city.html", city=city_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
