249329007e1f47c8b59f8b0f240769cfa8b252b0
Nikola Nedanov <107359038+Nedanovx@users.noreply.github.com>
2025-08-13 00:01:38 +0300

    modified views

--- Weather-Task 4/weatherapp/weather/views.py
+++ Weather-Task 4/weatherapp/weather/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import utils
 
-TEMPLATE_DIR = (
-    'os.path.join(BASE_DIR, "templates"),'
-)
+TEMPLATE_DIR = ('os.path.join(BASE_DIR, "templates"),')
 def index(request):
-    return render (request, "index.html")
+    cities = utils.get_five_cities()
+    stats = utils.get_stats(cities)
+    return render(request, "index.html", {"cities": cities, "stats": stats})
