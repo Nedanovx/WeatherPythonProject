eb5a24b14fa6370b7dea57bd61573d7e8c51c32f
Nikola Nedanov <107359038+Nedanovx@users.noreply.github.com>
2025-08-12 22:21:14 +0300

    added index

--- Weather-Task 4/weatherapp/weather/views.py
+++ Weather-Task 4/weatherapp/weather/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+TEMPLATE_DIR = (
+    'os.path.join(BASE_DIR, "templates"),'
+)
+def index(request):
+    return render (request, "index.html")
