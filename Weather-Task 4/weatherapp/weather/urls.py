636f4c7b8f4f67e565f1717facd66df0d40b3925
Nikola Nedanov <107359038+Nedanovx@users.noreply.github.com>
2025-08-13 01:20:34 +0300

    added search and error path

--- Weather-Task 4/weatherapp/weather/urls.py
+++ Weather-Task 4/weatherapp/weather/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.index, name='index'),
-]
+urlpatterns = [path('', views.index, name='index'),
+               path('search/', views.search_city_view, name="search_city_view"),
+               path('error/', views.city_not_found, name="city_not_found"),]
